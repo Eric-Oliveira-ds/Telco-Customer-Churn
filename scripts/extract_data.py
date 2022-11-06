cdfa39fab06e3dd3baf033fcf6e3bc4137af1ef5
Eric Oliveira <eric.oliveira1717@gmail.com>
2022-11-06 17:31:17 -0300

    update function for return curated parquet data

--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -1,16 +1,25 @@
 import pandas as pd
 import sys
 
-def read_csv_data(data_path:str)-> pd.DataFrame:
+def read_data(data_path:str)-> pd.DataFrame:
 
-    """ Funtion - Read csv file from path
+    """ Funtion - Read csv file from path 
+    and tranform column and save to parquet 
+
         Receive:
             data_path - path by csv data
         Return: 
             dataframe pandas
     """
+    
     df = pd.read_csv(data_path)
 
-    return df
+    df['TotalCharges'] = df['TotalCharges'].apply(lambda x: float(x.replace(' ', '0')))
+
+    df.to_parquet(r'C:\Users\erico\OneDrive\Documentos\Projeto-Eric-Churn\Telco-Customer-Churn\data\curated_data.parquet')
+
+    dataframe = pd.read_parquet(r'C:\Users\erico\OneDrive\Documentos\Projeto-Eric-Churn\Telco-Customer-Churn\data\curated_data.parquet')
+
+    return dataframe
 
     
