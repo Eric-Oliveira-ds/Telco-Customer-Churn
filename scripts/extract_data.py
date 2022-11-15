80ad4ddfdc4a958905a3f108536737bafa72f267
Eric Oliveira <eric.oliveira1717@gmail.com>
2022-11-15 17:59:28 -0300

    update notebook for extract data and function about SeniorCitizen categorical

--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import sys
 
 def read_data(data_path:str)-> pd.DataFrame:
 
@@ -15,6 +14,7 @@ def read_data(data_path:str)-> pd.DataFrame:
     df = pd.read_csv(data_path)
 
     df['TotalCharges'] = df['TotalCharges'].apply(lambda x: float(x.replace(' ', '0')))
+    df['SeniorCitizen'] = df['SeniorCitizen'].map({0:'No',1:'Yes'})
 
     df.to_parquet(r'C:\Users\erico\OneDrive\Documentos\Projeto-Eric-Churn\Telco-Customer-Churn\data\curated_data.parquet')
 
