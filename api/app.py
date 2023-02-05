c9f5a79feb04510bc739eaa0dc3d731d93a6afff
eric <ericoliveira1717@gmail.com>
2023-02-05 14:11:22 -0300

    end v1 app deploy

--- api/app.py
+++ api/app.py
@@ -3,66 +3,70 @@ from flask import Flask, request, render_template
 import joblib
 import pandas as pd
 import numpy as np
-import json
 
 # Load model
 model = joblib.load(open(r'C:\Users\erico\Documents\projeto-churn\Telco-Customer-Churn\models\model.pkl','rb'))
 
 app = Flask(__name__)
 
+@app.route('/')
 def predict():
     return render_template('predict.html')
-
-pred_proba = " "
 
 @app.route('/predict',methods=['GET','POST'])
 def home():
 
-    global pred_proba
+    pred_proba = 0.0
+    df = pd.DataFrame()
 
     if request.method =='POST':
 
-        SeniorCitizen = str(request.form['SeniorCitizen'])
-        Partner = str(request.form['Partner'])
-        Dependents = str(request.form['Dependents'])
-        tenure = int(request.form['tenure'])
-        MultipleLines = str(request.form['MultipleLines'])
-        InternetService = str(request.form['InternetService'])
-        OnlineSecurity = str(request.form['OnlineSecurity'])
-        OnlineBackup = str(request.form['OnlineBackup'])
-        DeviceProtection = str(request.form['DeviceProtection'])
-        TechSupport = str(request.form['TechSupport'])
-        StreamingTV = str(request.form['StreamingTV'])
-        StreamingMovies = str(request.form['StreamingMovies'])
-        Contract = str(request.form['Contract'])
-        PaperlessBilling = str(request.form['PaperlessBilling'])
-        PaymentMethod = str(request.form['PaymentMethod'])
-        MonthlyCharges = float(request.form['MonthlyCharges'])
-        TotalCharges = float(request.form['TotalCharges'])
+        try:
+            SeniorCitizen = str(request.form['SeniorCitizen'])
+            Partner = str(request.form['Partner'])
+            Dependents = str(request.form['Dependents'])
+            tenure = int(request.form['tenure'])
+            MultipleLines = str(request.form['MultipleLines'])
+            InternetService = str(request.form['InternetService'])
+            OnlineSecurity = str(request.form['OnlineSecurity'])
+            OnlineBackup = str(request.form['OnlineBackup'])
+            DeviceProtection = str(request.form['DeviceProtection'])
+            TechSupport = str(request.form['TechSupport'])
+            StreamingTV = str(request.form['StreamingTV'])
+            StreamingMovies = str(request.form['StreamingMovies'])
+            Contract = str(request.form['Contract'])
+            PaperlessBilling = str(request.form['PaperlessBilling'])
+            PaymentMethod = str(request.form['PaymentMethod'])
+            MonthlyCharges = float(request.form['MonthlyCharges'])
+            TotalCharges = float(request.form['TotalCharges'])
 
-        data = {
-            'SeniorCitizen': [SeniorCitizen],
-            'Partner': [Partner],
-            'Dependents': [Dependents],
-            'tenure': [tenure],
-            'MultipleLines': [MultipleLines],
-            'InternetService': [InternetService],
-            'OnlineSecurity': [OnlineSecurity],
-            'OnlineBackup': [OnlineBackup],
-            'DeviceProtection': [DeviceProtection],
-            'TechSupport': [TechSupport],
-            'StreamingTV': [StreamingTV],
-            'StreamingMovies': [StreamingMovies],
-            'Contract': [Contract],
-            'PaperlessBilling': [PaperlessBilling],
-            'PaymentMethod': [PaymentMethod],
-            'MonthlyCharges': [MonthlyCharges],
-            'TotalCharges': [TotalCharges]
-        }
+            data = {
+                'SeniorCitizen': [SeniorCitizen],
+                'Partner': [Partner],
+                'Dependents': [Dependents],
+                'tenure': [tenure],
+                'MultipleLines': [MultipleLines],
+                'InternetService': [InternetService],
+                'OnlineSecurity': [OnlineSecurity],
+                'OnlineBackup': [OnlineBackup],
+                'DeviceProtection': [DeviceProtection],
+                'TechSupport': [TechSupport],
+                'StreamingTV': [StreamingTV],
+                'StreamingMovies': [StreamingMovies],
+                'Contract': [Contract],
+                'PaperlessBilling': [PaperlessBilling],
+                'PaymentMethod': [PaymentMethod],
+                'MonthlyCharges': [MonthlyCharges],
+                'TotalCharges': [TotalCharges]
+            }
 
-        df = pd.DataFrame(data)
+            df = pd.DataFrame(data)
+            pred_proba = model.predict_proba(df)[:, 1]
+            pred_proba = np.around(pred_proba, decimals=2)
+            
+        except ValueError:
+            print('Enter a valid values please!')
 
-        pred_proba = model.predict_proba(df)[:, 1]
 
     return render_template('predict.html', prediction = pred_proba)
 
@@ -76,13 +80,13 @@ def predict_csv():
 
         pred_proba = model.predict_proba(df1)[:, 1]
 
-        df1["Prediction"] = pred_proba
-        df1["Prescription"] = np.where(pred_proba >= 0.50, "Potential Churn - Red alert", "No Potential Churn")
+        df1["Prediction"] = np.around(pred_proba, decimals=2)
+        df1["Prescription"] = np.where(pred_proba >= 0.70, "High Potential Churn - Red alert",
+                              np.where(pred_proba >= 0.50, "Moderate Potential Churn - Yellow alert", "No Potential Churn"))
         df1 = df1.reset_index(drop=True)
 
-        #response = df1.to_dict(orient="records")
+        return df1.to_html()
 
-        return df1.to_html() #json.dumps(response)
     return '''
         <form action="" method="post" enctype="multipart/form-data">
             <input type="file" name="file">
@@ -90,5 +94,6 @@ def predict_csv():
         </form>
     '''
 
+    
 if __name__=='__main__':
     app.run(host='0.0.0.0')
